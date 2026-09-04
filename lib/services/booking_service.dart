000558d0f090ac42702ca80b1fe1e9aa7ad1ceb7
George shaheen <geoege.tawam.alshahin@gmail.com>
2026-09-04 13:57:04 +0400

    refactor: use shared Firestore collection constants

--- lib/services/booking_service.dart
+++ lib/services/booking_service.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../core/firestore_collections.dart';
+
 class BookingService {
   BookingService(this._firebaseAuth, this._firestore);
 
@@ -10,21 +12,21 @@ class BookingService {
   User? get currentUser => _firebaseAuth.currentUser;
 
   Future<Map<String, dynamic>> loadUserProfile(String userId) async {
-    final snapshot = await _firestore.collection('users').doc(userId).get();
+    final snapshot = await _firestore.collection(FirestoreCollections.users).doc(userId).get();
     return snapshot.data() ?? <String, dynamic>{};
   }
 
   Future<DocumentReference<Map<String, dynamic>>> createBookingRequest(
     Map<String, dynamic> data,
   ) {
-    return _firestore.collection('shipment_requests').add(data);
+    return _firestore.collection(FirestoreCollections.shipmentRequests).add(data);
   }
 
   Stream<QuerySnapshot<Map<String, dynamic>>> watchBookingRequests(
     String userId,
   ) {
     return _firestore
-        .collection('shipment_requests')
+        .collection(FirestoreCollections.shipmentRequests)
         .where('userId', isEqualTo: userId)
         .snapshots();
   }
