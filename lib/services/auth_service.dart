6972350e53db68aa8144e1a171c8096e661e1631
George shaheen <geoege.tawam.alshahin@gmail.com>
2026-09-04 12:54:22 +0400

    refactor: preserve registration in authentication service

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
-  AuthService(this._firebaseAuth);
+  AuthService(this._firebaseAuth, this._firestore);
 
   final FirebaseAuth _firebaseAuth;
+  final FirebaseFirestore _firestore;
 
   Future<UserCredential> signIn({
     required String email,
@@ -13,5 +15,33 @@ class AuthService {
       email: email,
       password: password,
     );
+  }
+
+  Future<void> createAccount({
+    required String name,
+    required String phone,
+    required String email,
+    required String password,
+  }) async {
+    final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+
+    await userCredential.user?.updateDisplayName(name);
+    await _firestore.collection('users').doc(userCredential.user!.uid).set({
+      'name': name,
+      'phone': phone,
+      'email': email,
+      'role': 'customer',
+      'customerId':
+          'TW-${userCredential.user!.uid.substring(0, 8).toUpperCase()}',
+      'createdAt': FieldValue.serverTimestamp(),
+    });
+    await _firebaseAuth.signOut();
+  }
+
+  Future<void> sendPasswordResetEmail({required String email}) {
+    return _firebaseAuth.sendPasswordResetEmail(email: email);
   }
 }
