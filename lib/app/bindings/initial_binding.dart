3b885f18bc2bf29498ca3c2405f09b7354c30814
George shaheen <geoege.tawam.alshahin@gmail.com>
2026-09-04 12:44:50 +0400

    refactor: add empty GetX initial binding

---
+++ lib/app/bindings/initial_binding.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+/// Application-wide GetX registrations will be added here incrementally.
+///
+/// Keeping the initial binding empty preserves the current application
+/// behavior while establishing the MVC migration entry point.
+class InitialBinding extends Bindings {
+  @override
+  void dependencies() {}
+}
