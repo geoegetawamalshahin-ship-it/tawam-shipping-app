50b40a3872916976eeeb33874a1350ce1d2e527c
George shaheen <geoege.tawam.alshahin@gmail.com>
2026-09-08 20:17:37 +0400

    refactor: reuse shipping form header

--- lib/screens/international_moving_screen.dart
+++ lib/screens/international_moving_screen.dart
@@ -422,89 +422,22 @@ class _InternationalMovingScreenState extends State<InternationalMovingScreen> {
   Widget _buildHeader(BuildContext context) {
     final l10n = AppLocalizations.of(context)!;
 
-    return Container(
-      height: 82,
-      padding: const EdgeInsets.symmetric(horizontal: 14),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: const Border(bottom: BorderSide(color: _border)),
-        boxShadow: [
-          BoxShadow(
-            color: _deepBlue.withValues(alpha: .035),
-            blurRadius: 18,
-            offset: const Offset(0, 5),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          Material(
-            color: Colors.transparent,
-            child: InkWell(
-              onTap: () => Navigator.pop(context),
-              borderRadius: BorderRadius.circular(14),
-              child: Container(
-                width: 44,
-                height: 44,
-                decoration: BoxDecoration(
-                  color: _softGrey,
-                  borderRadius: BorderRadius.circular(14),
-                  border: Border.all(color: _border),
-                ),
-                child: const Icon(
-                  Icons.arrow_back_rounded,
-                  color: _deepBlue,
-                  size: 23,
-                ),
-              ),
-            ),
-          ),
-
-          const SizedBox(width: 14),
-
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  l10n.movingQuote,
-                  style: const TextStyle(
-                    color: _textDark,
-                    fontSize: 19,
-                    fontWeight: FontWeight.w900,
-                    letterSpacing: -.35,
-                  ),
-                ),
-                const SizedBox(height: 3),
-                Text(
-                  l10n.officialRateRequest,
-                  style: const TextStyle(
-                    color: _primaryBlue,
-                    fontSize: 8.2,
-                    fontWeight: FontWeight.w800,
-                    letterSpacing: .95,
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          Container(
-            width: 44,
-            height: 44,
-            decoration: BoxDecoration(
-              color: _softBlue,
-              borderRadius: BorderRadius.circular(14),
-            ),
-            child: const Icon(
-              Icons.home_work_outlined,
-              color: _primaryBlue,
-              size: 23,
-            ),
-          ),
-        ],
-      ),
+    return ShippingFormHeader(
+      title: l10n.movingQuote,
+      subtitle: l10n.officialRateRequest,
+      trailingIcon: Icons.home_work_outlined,
+      onBack: () => Navigator.pop(context),
+      borderColor: _border,
+      shadowColor: _deepBlue,
+      leadingBackgroundColor: _softGrey,
+      leadingIconColor: _deepBlue,
+      trailingBackgroundColor: _softBlue,
+      trailingIconColor: _primaryBlue,
+      titleColor: _textDark,
+      titleFontSize: 19,
+      subtitleFontSize: 8.2,
+      subtitleLetterSpacing: .95,
+      trailingIconSize: 23,
     );
   }
 
